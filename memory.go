ff5fe82f459954a3014da8c9c1a437df23b79022
Pere Negre <pnegre@esliceu.com>
2017-05-29 13:22:06 +0200

    Anem fent

--- memory.go
+++ memory.go
@@ -6,25 +6,25 @@ type Memory struct {
 
 
 func (self Memory) ReadByte(address uint16) byte {
-    return 0;
+    panic("ReadByte")
 }
 
 // ReadByteInternal reads a byte from address without taking
 // into account contention.
 func (self Memory) ReadByteInternal(address uint16) byte {
-    return 0;
+    return self.data[address]
 }
 
 // WriteByte writes a byte at address taking into account
 // contention.
 func (self Memory) WriteByte(address uint16, value byte) {
-
+    panic("WriteByte")
 }
 
 // WriteByteInternal writes a byte at address without taking
 // into account contention.
 func (self Memory) WriteByteInternal(address uint16, value byte) {
-
+    self.data[address] = value
 }
 
 // Follow contention methods. Leave unimplemented if you don't
@@ -51,11 +51,11 @@ func (self Memory) ContendWriteNoMreq_loop(address uint16, time int, count uint)
 }
 
 func (self Memory) Read(address uint16) byte {
-    panic("Not implemented")
+    panic("Memory.Read Not implemented")
 }
 
 func (self Memory) Write(address uint16, value byte, protectROM bool) {
-    panic("Not implemented")
+    panic("Memory.Write Not implemented")
 }
 
 // Data returns the memory content.
