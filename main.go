b6efd19c0b2419750e7090ae857e9c8b66add6a9
Pere Negre <pnegre@esliceu.com>
2017-05-30 22:21:42 +0200

    Anem fent

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ func main() {
 	cpuZ80.Reset()
 	cpuZ80.SetPC(0)
 	for {
+		pc := cpuZ80.PC()
+		instr, _, _ := z80.Disassemble(memory, pc, 0)
+		fmt.Printf("%04x: %s\n", pc, instr)
 		cpuZ80.DoOpcode()
-		fmt.Printf("PC = %04x\n", cpuZ80.PC())
+
 	}
 }
