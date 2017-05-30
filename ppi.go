c29b2eca57d6386fc9c03dfdd513c2f3ec0d3b51
Pere Negre <pnegre@esliceu.com>
2017-05-30 22:49:16 +0200

    Anem fent

--- ppi.go
+++ ppi.go
@@ -2,17 +2,23 @@ package main
 
 import "log"
 
-func ppi_writePort(ad byte, val byte) {
-    switch {
-    case ad == 0xab:
-        if (val & 0x80 != 0) {
-            log.Println("PPI initialization")
-            // PPI initialization
-            return
-        } else {
-            panic("PPI Write command register")
-        }
-    }
+var ppi_slots uint8
 
-    log.Fatalf("PPI: not implemented: %02x -> %02x", ad, val)
+func ppi_writePort(ad byte, val byte) {
+	switch {
+	case ad == 0xab:
+		if val&0x80 != 0 {
+			log.Println("PPI initialization")
+			// PPI initialization
+			return
+		} else {
+			panic("PPI Write command register")
+		}
+
+	case ad == 0xa8:
+		ppi_slots = val
+		return
+	}
+
+	log.Fatalf("PPI: not implemented: %02x -> %02x", ad, val)
 }
