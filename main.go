57283aa998a99022b6f57be462d5aed421667ade
Pere Negre <pnegre@esliceu.com>
2017-05-30 23:51:33 +0200

    Anem fent

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "github.com/remogatto/z80"
 import "log"
 
+const logAssembler = 0
+
 func main() {
 	memory := NewMemory("hb-501p_basic-bios1.rom")
 	ports := new(Ports)
@@ -11,9 +13,11 @@ func main() {
 	cpuZ80.SetPC(0)
 	log.Println("Beginning simulation...")
 	for {
-		//pc := cpuZ80.PC()
-		//instr, _, _ := z80.Disassemble(memory, pc, 0)
-		//log.Printf("%04x: %s\n", pc, instr)
+		if logAssembler != 0 {
+			pc := cpuZ80.PC()
+			instr, _, _ := z80.Disassemble(memory, pc, 0)
+			log.Printf("%04x: %s\n", pc, instr)
+		}
 		cpuZ80.DoOpcode()
 
 	}
