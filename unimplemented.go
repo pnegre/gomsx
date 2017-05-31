b6eab4ebdf465bb6fa171d3f530d531de9e69a08
Pere Negre <pnegre@esliceu.com>
2017-05-31 11:18:59 +0200

    Anem fent

---
+++ unimplemented.go
@@ -0,0 +1,40 @@
+package main
+
+// Mètodes no implementats //////////////////////////
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// Follow contention methods. Leave unimplemented if you don't
+// care about memory contention.
+
+// ContendRead increments the Tstates counter by time as a
+// result of a memory read at the given address.
+func (self *Memory) ContendRead(address uint16, time int) {
+	//panic("ContendRead not implemented")
+}
+
+func (self *Memory) ContendReadNoMreq(address uint16, time int) {
+	//panic("ContendReadNoMreq not implemented")
+}
+func (self *Memory) ContendReadNoMreq_loop(address uint16, time int, count uint) {
+	///panic("ContendReadNoMreq_loop not implemented")
+}
+
+func (self *Memory) ContendWriteNoMreq(address uint16, time int) {
+	//panic("ContendWriteNoMreq not implemented")
+}
+func (self *Memory) ContendWriteNoMreq_loop(address uint16, time int, count uint) {
+	//panic("ContendWriteNoMreq_loop not implemented")
+}
+
+func (self *Memory) Read(address uint16) byte {
+	panic("Memory.Read Not implemented")
+}
+
+func (self *Memory) Write(address uint16, value byte, protectROM bool) {
+	panic("Memory.Write Not implemented")
+}
+
+// Data returns the memory content.
+func (self *Memory) Data() []byte {
+	return nil
+}
