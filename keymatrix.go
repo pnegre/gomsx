1d1ac87becc0e8808e9495246abc6d63598371c8
pnegre <pere.negre@gmail.com>
2017-06-01 21:40:37 +0200

    Anem fent

--- keymatrix.go
+++ keymatrix.go
@@ -8,13 +8,13 @@ func init() {
 	// Mirar http://map.grauw.nl/articles/keymatrix.php
 	data = [][]int{
 		{
-			gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, // 7 &	6 ^	5 %	4 $	3 #	2 @	1 !	0 )
+			gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, // 7 &	6 ^	5 %	4 $	3 #	2 @	1 !	0 )
 		},
 		{
-			gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, // ; :	] }	[ {	\ ¦	= +	- _	9 (	8 *
+			gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, // ; :	] }	[ {	\ ¦	= +	- _	9 (	8 *
 		},
 		{
-			gogame.K_B, gogame.K_A, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, // B	A	DEAD	/ ?	. >	, <	` ~	' "
+			gogame.K_B, gogame.K_A, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, // B	A	DEAD	/ ?	. >	, <	` ~	' "
 		},
 		{
 			gogame.K_J, gogame.K_I, gogame.K_H, gogame.K_G, gogame.K_F, gogame.K_E, gogame.K_D, gogame.K_C,
@@ -26,19 +26,19 @@ func init() {
 			gogame.K_Z, gogame.K_Y, gogame.K_X, gogame.K_W, gogame.K_V, gogame.K_U, gogame.K_T, gogame.K_S,
 		},
 		{
-			gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, // F3 F F1 CODE CAPS GRAPH CTRL SHIFT
+			gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_LCTRL, gogame.K_LSHIFT, // F3 F F1 CODE CAPS GRAPH CTRL SHIFT
 		},
 		{
-			gogame.K_RETURN, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, // RET SELECT BS STOP TAB ESC F5 F4
+			gogame.K_RETURN, gogame.K_F11, gogame.K_BACK, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, // RET SELECT BS STOP TAB ESC F5 F4
 		},
 		{
-			gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_SPACE, // → ↓ ↑ ← DEL INS HOME SPACE
+			gogame.K_RIGHT, gogame.K_DOWN, gogame.K_UP, gogame.K_LEFT, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_SPACE, // → ↓ ↑ ← DEL INS HOME SPACE
 		},
 		{
-			gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, // NUM4	NUM3	NUM2	NUM1	NUM0	NUM/	NUM+	NUM*
+			gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, // NUM4	NUM3	NUM2	NUM1	NUM0	NUM/	NUM+	NUM*
 		},
 		{
-			gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, // NUM.	NUM,	NUM-	NUM9	NUM8	NUM7	NUM6	NUM5
+			gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, gogame.K_F11, // NUM.	NUM,	NUM-	NUM9	NUM8	NUM7	NUM6	NUM5
 		},
 	}
 }
