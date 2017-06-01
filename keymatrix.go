7980b4ec4ff7a8924dedfe65dc9ed9ec3c5f4543
Pere Negre <pnegre@esliceu.com>
2017-06-01 16:49:22 +0200

    Anem fent

--- keymatrix.go
+++ keymatrix.go
@@ -6,13 +6,13 @@ func keyMatrix(row byte) (result byte) {
 	// Mirar http://map.grauw.nl/articles/keymatrix.php
 	data := [][]int{
 		{
-			gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP,
+			gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, // 7 &	6 ^	5 %	4 $	3 #	2 @	1 !	0 )
 		},
 		{
-			gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP,
+			gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, // ; :	] }	[ {	\ ¦	= +	- _	9 (	8 *
 		},
 		{
-			gogame.K_B, gogame.K_A, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP,
+			gogame.K_B, gogame.K_A, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, // B	A	DEAD	/ ?	. >	, <	` ~	' "
 		},
 		{
 			gogame.K_J, gogame.K_I, gogame.K_H, gogame.K_G, gogame.K_F, gogame.K_E, gogame.K_D, gogame.K_C,
@@ -23,17 +23,32 @@ func keyMatrix(row byte) (result byte) {
 		{
 			gogame.K_Z, gogame.K_Y, gogame.K_X, gogame.K_W, gogame.K_V, gogame.K_U, gogame.K_T, gogame.K_S,
 		},
+		{
+			gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, // F3 F F1 CODE CAPS GRAPH CTRL SHIFT
+		},
+		{
+			gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, // RET SELECT BS STOP TAB ESC F5 F4
+		},
+		{
+			gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_SPACE, // → ↓ ↑ ← DEL INS HOME SPACE
+		},
+		{
+			gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, // NUM4	NUM3	NUM2	NUM1	NUM0	NUM/	NUM+	NUM*
+		},
+		{
+			gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, gogame.K_UP, // NUM.	NUM,	NUM-	NUM9	NUM8	NUM7	NUM6	NUM5
+		},
 	}
 
 	result = 0xff
-	switch {
-	case row < 6:
+	if row < 11 {
 		for i := 0; i < 8; i++ {
 			if gogame.IsKeyPressed(data[row][i]) {
 				result &= ^byte(1 << byte(7-i))
 			}
 		}
-
+	} else {
+		panic("KeyMatrix: Tried to scan row > 10")
 	}
 	return
 }
