ef0a756cd3a206a17f6febe13001b516804131c3
pnegre <pere.negre@gmail.com>
2017-06-03 01:36:09 +0200

    Anem fent

--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import "github.com/remogatto/z80"
 import "github.com/pnegre/gogame"
 import "log"
-import "time"
+
+// import "time"
 import "os"
 import "bufio"
 
 const (
-	WINTITLE  = "gomsx"
-	WIN_W     = 800
-	WIN_H     = 600
-	ROMFILE   = "msx1.rom"
-	NANOS_SCR = 20000000 // 50Hz -> Interval de 20Mseg
+	WINTITLE        = "gomsx"
+	WIN_W           = 800
+	WIN_H           = 600
+	ROMFILE         = "msx1.rom"
+	NANOS_SCR       = 20000000 // 50Hz -> Interval de 20Mseg
+	TSTATESPERFRAME = 71400
 )
 
 func main() {
@@ -28,11 +30,13 @@ func main() {
 	cpuZ80.Reset()
 	cpuZ80.SetPC(0)
 	log.Println("Beginning simulation...")
-	lastTm := time.Now().UnixNano()
-	delta := int64(0)
+	// lastTm := time.Now().UnixNano()
+	// delta := int64(0)
 	logAssembler := false
 	for {
-		for i := 0; i < 500; i++ {
+		// EL z80 executa devers 580000 instr per segon
+		// Un "frame" són 20mseg, per tant executa 11600 instr. per frame
+		for i := 0; i < 11600; i++ {
 			if logAssembler {
 				pc := cpuZ80.PC()
 				instr, _, _ := z80.Disassemble(memory, pc, 0)
@@ -45,19 +49,15 @@ func main() {
 			logAssembler = true
 		}
 
-		delta = time.Now().UnixNano() - lastTm
-		if delta > NANOS_SCR {
-			if quit := gogame.SlurpEvents(); quit == true {
-				break
-			}
-
-			graphics_renderScreen()
-			if vdp_enabledInterrupts {
-				cpuZ80.Interrupt()
-			}
-			lastTm = time.Now().UnixNano()
-			gogame.Delay(1)
+		if quit := gogame.SlurpEvents(); quit == true {
+			break
 		}
+
+		graphics_renderScreen()
+		if vdp_enabledInterrupts {
+			cpuZ80.Interrupt()
+		}
+		gogame.Delay(20)
 	}
 }
 
@@ -69,12 +69,12 @@ func loadROMS(memory *Memory) {
 	memory.load(buffer, 0, 0)
 	memory.load(buffer[0x4000:], 1, 0)
 
-	buffer, err = readFile("caos.rom")
-	if err != nil {
-		log.Fatal(err)
-	}
-	memory.load(buffer, 1, 1)
-	memory.load(buffer[0x4000:], 2, 1)
+	// buffer, err = readFile("caos.rom")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// memory.load(buffer, 1, 1)
+	// memory.load(buffer[0x4000:], 2, 1)
 }
 
 func readFile(fname string) ([]byte, error) {
