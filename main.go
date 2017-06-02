96045f4a24776697fd3e013de1455d34066ab844
pnegre <pere.negre@gmail.com>
2017-06-02 20:01:14 +0200

    Començat el tema dels SLOTS

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "github.com/remogatto/z80"
 import "github.com/pnegre/gogame"
 import "log"
 import "time"
+import "os"
+import "bufio"
 
 const (
 	WINTITLE  = "gomsx"
@@ -13,6 +15,26 @@ const (
 	NANOS_SCR = 20000000 // 50Hz -> Interval de 20Mseg
 )
 
+func readFile(fname string) ([]byte, error) {
+	f, err := os.Open(fname)
+	if err != nil {
+		return nil, err
+	}
+
+	stats, statsErr := f.Stat()
+	if statsErr != nil {
+		return nil, statsErr
+	}
+
+	var size int64 = stats.Size()
+	bytes := make([]byte, size)
+
+	bufr := bufio.NewReader(f)
+	_, err = bufr.Read(bytes)
+
+	return bytes, err
+}
+
 func main() {
 	if err := gogame.Init(WINTITLE, WIN_W, WIN_H); err != nil {
 		log.Fatal(err)
@@ -20,7 +42,13 @@ func main() {
 	defer gogame.Quit()
 
 	memory := NewMemory()
-	memory.loadFromFile(ROMFILE)
+	buffer, err := readFile(ROMFILE)
+	if err != nil {
+		log.Fatal(err)
+	}
+	memory.load(buffer, 0, 0)
+	memory.load(buffer[0x4000:], 1, 0)
+
 	ports := new(Ports)
 	cpuZ80 := z80.NewZ80(memory, ports)
 	cpuZ80.Reset()
--- memory.go
+++ memory.go
@@ -1,9 +1,10 @@
 package main
 
-import "os"
-
 type Memory struct {
-	data [0x10000]byte
+	page0 [4][0x4000]byte
+	page1 [4][0x4000]byte
+	page2 [4][0x4000]byte
+	page3 [4][0x4000]byte
 }
 
 func NewMemory() *Memory {
@@ -11,15 +12,19 @@ func NewMemory() *Memory {
 	return mem
 }
 
-func (self *Memory) loadFromFile(romFile string) error {
-	f, err := os.Open(romFile)
-	if err != nil {
-		return err
-	}
+// Loads 16k (one page)
+func (self *Memory) load(data []byte, page, slot int) {
+	switch page {
+	case 0:
+		copy(self.page0[slot][:], data[:0x4000])
+	case 1:
+		copy(self.page1[slot][:], data[:0x4000])
+	case 2:
+		copy(self.page2[slot][:], data[:0x4000])
+	case 3:
+		copy(self.page3[slot][:], data[:0x4000])
 
-	f.Read(self.data[:32768]) //32k
-	f.Close()
-	return nil
+	}
 }
 
 func (self *Memory) ReadByte(address uint16) byte {
@@ -29,7 +34,22 @@ func (self *Memory) ReadByte(address uint16) byte {
 // ReadByteInternal reads a byte from address without taking
 // into account contention.
 func (self *Memory) ReadByteInternal(address uint16) byte {
-	return self.data[address]
+	pg0Slot := ppi_slots & 0x03
+	pg1Slot := (ppi_slots & 0x0C) >> 2
+	pg2Slot := (ppi_slots & 0x30) >> 4
+	pg3Slot := (ppi_slots & 0xC0) >> 6
+	page := address / 0x4000
+	switch page {
+	case 0:
+		return self.page0[pg0Slot][address]
+	case 1:
+		return self.page1[pg1Slot][address-0x4000]
+	case 2:
+		return self.page2[pg2Slot][address-0x8000]
+	case 3:
+		return self.page3[pg3Slot][address-0xC000]
+	}
+	panic("Tried to read impossible memory location")
 }
 
 // WriteByte writes a byte at address taking into account
@@ -41,5 +61,24 @@ func (self *Memory) WriteByte(address uint16, value byte) {
 // WriteByteInternal writes a byte at address without taking
 // into account contention.
 func (self *Memory) WriteByteInternal(address uint16, value byte) {
-	self.data[address] = value
+	pg0Slot := ppi_slots & 0x03
+	pg1Slot := (ppi_slots & 0x0C) >> 2
+	pg2Slot := (ppi_slots & 0x30) >> 4
+	pg3Slot := (ppi_slots & 0xC0) >> 6
+	page := address / 0x4000
+	switch page {
+	case 0:
+		self.page0[pg0Slot][address] = value
+		return
+	case 1:
+		self.page1[pg1Slot][address-0x4000] = value
+		return
+	case 2:
+		self.page2[pg2Slot][address-0x8000] = value
+		return
+	case 3:
+		self.page3[pg3Slot][address-0xC000] = value
+		return
+	}
+	panic("Tried to read impossible memory location")
 }
