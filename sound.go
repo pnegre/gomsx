4b1d2b334d617353f80168c63e1ee28785ca5099
Pere Negre <pnegre@esliceu.com>
2017-06-06 10:17:27 +0200

    Anem fent

--- sound.go
+++ sound.go
@@ -28,9 +28,15 @@ func sound_readPort(ad byte) byte {
 	switch {
 	case ad == 0xa2:
 		// Read value from port
-		if sound_regNext == 0x0e || sound_regNext == 0x0f {
-			// Ignoring joysticks...
+		if sound_regNext == 0x0e {
+			// joystick triggers.
+			// Per ara ho posem a 1 (no moviment de joystick)
 			return 0x3f
+		}
+		if sound_regNext == 0x0f {
+			// PSG port 15 (joystick select)
+			// TODO: millorar
+			return 0
 		}
 		return sound_regs[sound_regNext]
 	}
