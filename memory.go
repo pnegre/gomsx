87321673d0eadd6a11f16b3ac374227a17242c68
Pere Negre <pnegre@esliceu.com>
2017-06-08 11:23:10 +0200

    Anem fent

--- memory.go
+++ memory.go
@@ -8,11 +8,7 @@ type Mapper interface {
 }
 
 type Memory struct {
-	contents [4][4][0x4000]byte
-	// page0      [4][0x4000]byte
-	// page1      [4][0x4000]byte
-	// page2      [4][0x4000]byte
-	// page3      [4][0x4000]byte
+	contents   [4][4][0x4000]byte
 	mapper     Mapper
 	slotMapper int
 }
@@ -27,20 +23,6 @@ func NewMemory() *Memory {
 // Loads 16k (one page)
 func (self *Memory) load(data []byte, page, slot int) {
 	copy(self.contents[page][slot][:], data[:0x4000])
-	// switch page {
-	// case 0:
-	// 	copy(self.page0[slot][:], data[:0x4000])
-	// 	return
-	// case 1:
-	// 	copy(self.page1[slot][:], data[:0x4000])
-	// 	return
-	// case 2:
-	// 	copy(self.page2[slot][:], data[:0x4000])
-	// 	return
-	// case 3:
-	// 	copy(self.page3[slot][:], data[:0x4000])
-	// 	return
-	// }
 }
 
 func (self *Memory) setMapper(mapper Mapper, slot int) {
@@ -61,32 +43,40 @@ func (self *Memory) ReadByteInternal(address uint16) byte {
 	// 	return self.ffff
 	// }
 
-	pg0Slot := ppi_slots & 0x03
-	pg1Slot := (ppi_slots & 0x0C) >> 2
-	pg2Slot := (ppi_slots & 0x30) >> 4
-	pg3Slot := (ppi_slots & 0xC0) >> 6
+	pgSlots := []byte{
+		ppi_slots & 0x03,
+		(ppi_slots & 0x0C) >> 2,
+		(ppi_slots & 0x30) >> 4,
+		(ppi_slots & 0xC0) >> 6,
+	}
+	// pg0Slot := ppi_slots & 0x03
+	// pg1Slot := (ppi_slots & 0x0C) >> 2
+	// pg2Slot := (ppi_slots & 0x30) >> 4
+	// pg3Slot := (ppi_slots & 0xC0) >> 6
 
 	if self.mapper != nil && address >= 0x4000 && address <= 0xBFFF {
-		if address < 0x8000 && self.slotMapper == int(pg1Slot) {
+		if address < 0x8000 && self.slotMapper == int(pgSlots[1]) {
 			return self.mapper.readByte(address)
 		}
-		if address < 0xC000 && self.slotMapper == int(pg2Slot) {
+		if address < 0xC000 && self.slotMapper == int(pgSlots[2]) {
 			return self.mapper.readByte(address)
 		}
 	}
 
 	page := address / 0x4000
-	switch page {
-	case 0:
-		return self.contents[0][pg0Slot][address]
-	case 1:
-		return self.contents[1][pg1Slot][address-0x4000]
-	case 2:
-		return self.contents[2][pg2Slot][address-0x8000]
-	case 3:
-		return self.contents[3][pg3Slot][address-0xC000]
-	}
-	panic("Tried to read impossible memory location")
+	delta := address - page*0x4000
+	return self.contents[page][pgSlots[page]][delta]
+	// switch page {
+	// case 0:
+	// 	return self.contents[0][pg0Slot][address]
+	// case 1:
+	// 	return self.contents[1][pg1Slot][address-0x4000]
+	// case 2:
+	// 	return self.contents[2][pg2Slot][address-0x8000]
+	// case 3:
+	// 	return self.contents[3][pg3Slot][address-0xC000]
+	// }
+	// panic("Tried to read impossible memory location")
 }
 
 // WriteByte writes a byte at address taking into account
