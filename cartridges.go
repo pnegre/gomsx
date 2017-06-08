eaccd0f6bc2c1312119189cfcdfad49efc063edd
Pere Negre <pnegre@esliceu.com>
2017-06-08 13:31:59 +0200

    Carreguem nemesis2, encara que hi ha fallos...

--- cartridges.go
+++ cartridges.go
@@ -1,6 +1,7 @@
 package main
 
 import "regexp"
+import "log"
 
 const (
 	UNKNOWN = iota
@@ -54,8 +55,10 @@ func (self *MapperKonami4) writeByte(address uint16, value byte) {
 }
 
 type MapperKonami5 struct {
-	contents []byte
-	sels     [4]int
+	contents    []byte
+	sels        [4]int
+	sccActive   bool
+	sccContents [0x800]byte
 }
 
 func NewMapperKonami5(data []byte) Mapper {
@@ -64,10 +67,17 @@ func NewMapperKonami5(data []byte) Mapper {
 		m.sels[i] = i
 	}
 	m.contents = data
+	m.sccActive = false
 	return m
 }
 
 func (self *MapperKonami5) readByte(address uint16) byte {
+	if self.sccActive && address >= 0x9800 && address <= 0x9fff {
+		return self.sccContents[address-0x9800]
+	}
+	if false {
+		log.Printf("Konami5 read: %04x sels: %v\n", address, self.sels)
+	}
 	address -= 0x4000
 	place := address / 0x2000
 	realMem := self.contents[self.sels[place]*0x2000:]
@@ -76,18 +86,25 @@ func (self *MapperKonami5) readByte(address uint16) byte {
 }
 
 func (self *MapperKonami5) writeByte(address uint16, value byte) {
+	if self.sccActive && address >= 0x9800 && address <= 0x9fff {
+		self.sccContents[address-0x9800] = value
+	}
 	switch {
 	case address >= 0x5000 && address <= 0x57ff:
-		self.sels[0] = int(value)
+		self.sels[0] = int(value % 16)
 		return
 	case address >= 0x7000 && address <= 0x77ff:
-		self.sels[1] = int(value)
+		self.sels[1] = int(value % 16)
 		return
 	case address >= 0x9000 && address <= 0x97ff:
-		self.sels[2] = int(value)
+		if (value & 0x3f) == 0x3f {
+			self.sccActive = true
+			return
+		}
+		self.sels[2] = int(value % 16)
 		return
 	case address >= 0xb000 && address <= 0xb7ff:
-		self.sels[3] = int(value)
+		self.sels[3] = int(value % 16)
 		return
 	}
 
