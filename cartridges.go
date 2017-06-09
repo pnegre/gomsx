d4c2de35f8ec149da6ae56c65e4bf93dc5265f22
pnegre <pere.negre@gmail.com>
2017-06-10 00:09:06 +0200

    Anem fent

--- cartridges.go
+++ cartridges.go
@@ -70,8 +70,9 @@ func (self *MapperKonami4) writeByte(address uint16, value byte) {
 
 type MapperKonami5 struct {
 	contents []byte
-	numBanks byte
+	numBanks int
 	sels     [4]int
+	scc      [0x800]byte
 }
 
 func NewMapperKonami5(data []byte) Mapper {
@@ -80,40 +81,53 @@ func NewMapperKonami5(data []byte) Mapper {
 		m.sels[i] = i
 	}
 	m.contents = data
-	m.numBanks = byte(len(data) / 8192)
+	m.numBanks = len(data) / 8192
 	return m
 }
 
 func (self *MapperKonami5) readByte(address uint16) byte {
+	if (self.sels[3]&0x3f == 0x3f) && address >= 0x9800 && address <= 0x9fff {
+		// SCC Area
+		return self.scc[address-0x9800]
+	}
 	address -= 0x4000
 	place := address / 0x2000
-	realMem := self.contents[self.sels[place]*0x2000:]
+	realMem := self.contents[(self.sels[place]%self.numBanks)*0x2000:]
 	delta := address - 0x2000*place
 	return realMem[delta]
 }
 
 func (self *MapperKonami5) writeByte(address uint16, value byte) {
 	switch {
+	case (self.sels[3]&0x3f == 0x3f) && address >= 0x9800 && address <= 0x9fff:
+		// SCC AREA
+		self.scc[address-0x9800] = value
+		return
+
 	case address >= 0x5000 && address <= 0x57ff:
-		self.sels[0] = int(value % self.numBanks)
+		self.sels[0] = int(value)
 		return
+
 	case address >= 0x7000 && address <= 0x77ff:
-		self.sels[1] = int(value % self.numBanks)
+		self.sels[1] = int(value)
 		return
+
 	case address >= 0x9000 && address <= 0x97ff:
-		self.sels[2] = int(value % self.numBanks)
+		self.sels[2] = int(value)
 		return
+
 	case address >= 0xb000 && address <= 0xb7ff:
-		self.sels[3] = int(value % self.numBanks)
+
+		self.sels[3] = int(value)
 		return
 	}
 
-	// TODO: Arreglar per SCC...
-	address -= 0x4000
-	place := address / 0x2000
-	realMem := self.contents[self.sels[place]*0x2000:]
-	delta := address - 0x2000*place
-	realMem[delta] = value
+	// // TODO: Arreglar per SCC...
+	// address -= 0x4000
+	// place := address / 0x2000
+	// realMem := self.contents[self.sels[place]*0x2000:]
+	// delta := address - 0x2000*place
+	// realMem[delta] = value
 }
 
 type MapperASCII8 struct {
