2f3600c11c81f206a3c53e4a7789788674f8d8d9
pnegre <pere.negre@gmail.com>
2017-07-13 21:41:26 +0200

    Anem fent

--- romdatabase.go
+++ romdatabase.go
@@ -5,6 +5,8 @@ import "encoding/xml"
 import "io/ioutil"
 import "errors"
 
+const XMLDATABASE = "softwaredb.xml"
+
 func searchInRomDatabase(romToSearch string) (string, error) {
 	type Softwaredb struct {
 		XMLName xml.Name `xml:"softwaredb"`
@@ -20,7 +22,7 @@ func searchInRomDatabase(romToSearch string) (string, error) {
 		} `xml:"software"`
 	}
 
-	bytes, err := ioutil.ReadFile("softwaredb.xml")
+	bytes, err := ioutil.ReadFile(XMLDATABASE)
 	if err != nil {
 		return "", err
 	}
