51a6839612cdeeeff0146dd7c65396fd7407b6bf
pnegre <pere.negre@gmail.com>
2017-08-03 11:31:45 +0200

    Tenim so SCC, però sempre amb ona triangular

--- scc.go
+++ scc.go
@@ -8,10 +8,11 @@ const (
 
 type SCCChannel struct {
 	waveform   []int
-	volume     byte
+	volume     float32
 	frequency  float32
 	sccRegFreq [2]byte
 	on         bool
+	count      int
 }
 
 var scc_channels []*SCCChannel
@@ -30,6 +31,29 @@ func (self *SCCChannel) feedSamples(data []int16) {
 	if !self.on {
 		return
 	}
+
+	if self.frequency > FREQUENCY/2 {
+		return
+	}
+
+	// Based on code from EMULIB, Sound.c, function RenderAudio()
+
+	K := int(0x10000 * self.frequency / FREQUENCY)
+	L1 := self.count
+	var A1 int16
+	for i := 0; i < len(data); i, L1 = i+1, L1+K {
+		L2 := L1 + K
+		if L1&0x8000 != 0 {
+			A1 = 127
+		} else {
+			A1 = -128
+		}
+		if (L1^L2)&0x8000 != 0 {
+			A1 = A1 * int16((0x8000-(L1&0x7FFF)-(L2&0x7FFF))/K)
+		}
+		data[i] += int16(float32(A1) * self.volume)
+	}
+	self.count = L1 & 0xFFFF
 }
 
 func scc_feedSamples(data []int16) {
@@ -60,7 +84,7 @@ func scc_write(n uint16, b byte) {
 	case n >= 0x8a && n < 0x8f:
 		// Volume
 		nch := n - 0x8a
-		scc_channels[nch].volume = b
+		scc_channels[nch].volume = float32(b)
 
 	case n == 0x8f:
 		// ON/OFF switch channel 1 to 5
